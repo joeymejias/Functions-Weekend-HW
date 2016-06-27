71ad80b10b5ddf86a471b955536a87a76c788a65
Joey <joeymejias@gmail.com>
2016-06-27 09:59:18 -0400

    all the problems

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -48,6 +48,15 @@ public class Main {
         System.out.println("Okay. Thanks, bro. Let me get one more int. (No error exception available.)");
         intB = scanner.nextInt();
         sumDouble(intA, intB);
+
+        //Problem 3
+        close10(5,6);
+
+        //Problem 4
+        frontBack("bacon");
+
+        //Problem 5
+        notString("bacon");
     }
 
     /**
@@ -96,5 +105,69 @@ public class Main {
             System.out.println(newSumDouble);
         }
         return newSumDouble;
+    }
+
+    /**
+     * Given 2 int values, return whichever value is nearest to the value 10, or
+     * return 0 in the event of a tie.
+     * Note that Math.abs(n) returns the absolute value of a number.
+
+     close10(8, 13) → 8
+     close10(13, 8) → 8
+     close10(13, 7) → 0
+     * @param a
+     * @param b
+     * @return
+     */
+    public static int close10(int a, int b) {
+        int aDiff = Math.abs(a - 10);
+        int bDiff = Math.abs(b - 10);
+
+        if (aDiff < bDiff) {
+            return a;
+        }
+        if (bDiff < aDiff) {
+            return b;
+        }
+        return 0;  // i.e. aDiff == bDiff
+
+        // Solution notes: aDiff/bDiff local vars clean the code up a bit.
+        // Could have "else" before the second if and the return 0.
+    }
+
+    /**
+     *Given a string, return a new string where the first and last chars have been exchanged.
+
+     frontBack("code") → "eodc"
+     frontBack("a") → "a"
+     frontBack("ab") → "ba"
+     */
+    public static String frontBack(String str) {
+        if (str.length() <= 1) return str;
+
+        String mid = str.substring(1, str.length()-1);
+
+        // last + mid + first
+        return str.charAt(str.length()-1) + mid + str.charAt(0);
+    }
+
+    /**
+     *
+     Given a string, return a new string where "not " has been added to the front.
+     However, if the string already begins with "not", return the string unchanged.
+     Note: use .equals() to compare 2 strings.
+
+     notString("candy") → "not candy"
+     notString("x") → "not x"
+     notString("not bad") → "not bad"
+     * @param str
+     * @return
+     */
+    public static String notString(String str) {
+        if (str.length() >= 3 && str.substring(0, 3).equals("not")) {
+            return str;
+        }
+
+        return "not " + str;
     }
 }
